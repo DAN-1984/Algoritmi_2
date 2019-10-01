29beb164a5c63f4fb89001d2d9d6d4c8859177c3
Dan_1984 <50488679+DAN-1984@users.noreply.github.com>
2019-10-01 12:52:50 +0500

    Algoritmi_2

---
+++ ConvertToBin/ConvertToBin.cpp
@@ -0,0 +1,33 @@
+﻿// ConvertToBin.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+// Реализовать функцию перевода чисел из десятичной системы в двоичную, используя рекурсию.
+
+#include <iostream>
+#include <stdio.h>
+
+void ConvertBin(__int64 N, __int64 i, __int64 & binary)
+{
+	if (N <= 0) return;
+	binary += (N % 2) * i;
+	N = N / 2;
+	i *= 10;
+	ConvertBin(N, i, binary);
+}
+
+using namespace std;
+int main()
+{
+	__int64 N;
+	while (1)
+	{
+		cout << "Enter number: ";
+		cin >> N; // при использовании scanf выдает ошибку
+		__int64 i = 1, binary = 0;
+		ConvertBin(N, i, binary);
+		cout << "Convert to bin: "<< binary << endl;
+	}
+	return 0;
+}
+
+
+
---
+++ Возведение в степень/Возведение в степень.cpp
@@ -0,0 +1,62 @@
+﻿// Возведение в степень.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+// Реализовать функцию возведения числа a в степень b:
+// а. Без рекурсии.
+// б. Рекурсивно.
+// с. *Рекурсивно, используя свойство чётности степени.
+#include <iostream>
+void Power(int b, int a) // Функция возведения в степень без рекурсии
+{
+	using namespace std;
+	cout << "Enter number: ";
+	cin >> a;
+	cout << "Enter power: ";
+	cin >> b;
+	int nsave = a;
+	if (b == 0) { cout << 1 << endl; }
+	for (int i = 1; i < b; i++)
+	{
+		a *= nsave;
+	}
+	cout << a << endl;
+}
+
+
+int RecursivePow(int base, int power) // Функция возведения в степень с рекурсией
+{
+	return power == 1 ? base : base * RecursivePow(base, power - 1);
+}
+
+void PowProperty(int i, int x, int y, int y1) // используя свойство чётности степени
+{
+	if (y > 2) {
+		x *= x;
+		y -= 2;
+		PowProperty(i, x, y, y1);
+	}
+	else if (y == 1) {
+		x *= i;
+		y--;
+		PowProperty(i, x, y, y1);
+	}
+	else {
+		std::cout << i << "^" << y1 << " = " << x;
+	}
+}
+using namespace std;
+int main()
+{
+	int b = 0, a = 0, i = 1, nsave = 0;
+	Power(b, a); // без рекурсии
+	cout << "Enter number: ";
+	cin >> a;
+	cout << "Enter power: ";
+	cin >> b;
+	std::cout << RecursivePow(a, b) << std::endl; // с рекурсией
+	PowProperty(a, a * a, b - 2, b); // используя свойство чётности степени
+	getchar();
+	getchar();
+	return 0;
+}
+
+
